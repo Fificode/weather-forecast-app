f70b85c58744bdb0c815c9ddbf4f20bd43b72dd4
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-07-27 02:40:58 +0100

    added hourforecast component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react'
 import Main from './components/Main';
 import Navbar from './components/Navbar';
 import Loading from './components/Loading';
+import Hourlychart from './components/Hourlychart';
 import './styles/navbar.css';
 import './styles/main.css';
 import './styles/loading.css';
+import './styles/hourlychart.css'
 
 
 
@@ -12,26 +14,45 @@ function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
    const [data, setData] = useState([]);
-   
+   const [hoursData, setHoursData] = useState([]);
 
-  //Weather conditions based on latitude and longitude
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
+     navigator.geolocation.getCurrentPosition(function (position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
 
+
+  //Weather conditions based on latitude and longitude
+  useEffect(() => {
+    const fetchData = async () => {
+    
       const url = `${process.env.REACT_APP_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${lat}&q=${long}&aqi=no`;
       await fetch(url)
       
         .then(res => res.json())
-        .then(result => {console.log(url);
+        .then(result => {
           setData(result)
          })
        
     }
     fetchData();
+  }, [lat, long]);
+
+  
+  //Weather conditions based on latitude and longitude hourly
+  useEffect(() => {
+    const fetchHourlyData = async () => {
+    
+      const url = `${process.env.REACT_APP_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${lat}&q=${long}&q=days&days=7&aqi=no&alerts=no`;
+      await fetch(url)
+      
+        .then(res => res.json())
+        .then(result => {console.log(url);
+          setHoursData(result)
+         })
+       
+    }
+    fetchHourlyData();
   }, [lat, long]);
 
  
@@ -41,6 +62,9 @@ function App() {
      
              <Navbar setData={setData} />
       {(typeof data.location != 'undefined') ? (<Main weatherData={data} />) : (<Loading />)}
+      
+        { hoursData.map((hourData) => (<Hourlychart hourData={hourData} key={hoursData.localtime_epoch} />)) }
+     
     </div>
   );
 }
---
+++ src/components/Hourlychart.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import moment from 'moment';
+
+const Hourlychart = ({hourData}) => {
+  return (
+     <div className='weather__hourly'>
+<div className="weather__hourly-container">
+    <div className="weather__hourly-image_container"><img src={hourData.current.condition.icon} alt="Weather condition icon" className='weather__hourly-image'/></div>
+    <div className="weather__hourly-date">
+
+<p>
+            {moment().format("h:mm a")}
+          </p>
+
+</div>
+<div className="weather__hourly-temperature">
+<h2>{hourData.current.temp_c.toFixed(0)}<span>&deg;C</span></h2>
+
+</div>
+
+</div>
+    </div>
+  )
+}
+
+export default Hourlychart
